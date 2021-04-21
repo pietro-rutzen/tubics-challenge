df1c88b269eac76c21ae9eca90ea38d0ab4aead8
Pietro Rutzen <pietro@gramadosummit.com>
2021-04-21 11:51:31 -0300

    Setting up middlewares and app structure.

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+require("./auth/passport");
+
+require("./models/user");
+
+const middlewares = require("./middlewares");
+const api = require("./api");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
+// showcase for tests
+app.get("/", (req, res) => {
+  res.json({
+    message: "✨🇧🇷✨",
+  });
+});
+
+app.use("/api/v1", api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
