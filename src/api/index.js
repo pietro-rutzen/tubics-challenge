28dae98d49aa670449c6e12d025c6b6ea78382e5
Pietro Rutzen <pietro@gramadosummit.com>
2021-04-24 19:26:39 -0300

    Resource protection.

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const registerApi = require("./register");
 const loginApi = require("./login");
+const middlewares = require("../middlewares");
 
 const router = express.Router();
 
-router.use(registerApi);
 router.use(loginApi);
+
+router.use(middlewares.checkAuth) // protects the routes below
+router.use(registerApi);
+//router.use(gameApi);
 
 module.exports = router;
