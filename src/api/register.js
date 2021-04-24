d9ea2615d3e60a0c7ea0fca246e4c325f326e984
Pietro Rutzen <pietro@gramadosummit.com>
2021-04-24 18:47:17 -0300

    Player controller.

--- src/api/register.js
+++ src/api/register.js
@@ -1,29 +1,8 @@
 const express = require("express");
-const Player = require("../models/player");
-const bcrypt = require("bcrypt");
-
+const { checkAuth } = require("../middlewares");
 const router = express.Router();
+const playerController = require('../controllers/player');
 
-router.post("/register", async (req, res) => {
-  const { fullName, email, password } = req.body;
-
-  const alreadyExistsPlayer = await Player.findOne({ where: { email } }).catch(
-    (err) => {
-      console.log("Error: ", err);
-    }
-  );
-
-  if (alreadyExistsPlayer) {
-    return res.status(409).json({ message: "Player with that email already exists!" });
-  }
-  let hashedPassword = await bcrypt.hash(password, 10)
-  const newPlayer = new Player({ fullName, email, password: hashedPassword });
-  const savedPlayer = await newPlayer.save().catch((err) => {
-    console.log("Error: ", err);
-    res.status(500).json({ error: "Cannot register player at the moment!" });
-  });
-
-  if (savedPlayer) res.json({ message: "Thanks for registering!" });
-});
+router.post("/register", checkAuth, playerController.register);
 
 module.exports = router;
---
+++ src/controllers/player.js
@@ -0,0 +1,27 @@
+const Player = require("../models/player");
+const bcrypt = require("bcrypt");
+
+
+exports.register = (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  const alreadyExistsPlayer = await Player.findOne({ where: { email } }).catch(
+    (err) => {
+      console.log("Error: ", err);
+    }
+  );
+
+  if (alreadyExistsPlayer) {
+    return res.status(409).json({ message: "Player with that email already exists!" });
+  }
+
+  let hashedPassword = await bcrypt.hash(password, 10)
+  const newPlayer = new Player({ fullName, email, password: hashedPassword });
+
+  const savedPlayer = await newPlayer.save().catch((err) => {
+    console.log("Error: ", err);
+    res.status(500).json({ error: "Cannot register player at the moment!" });
+  });
+  
+  if (savedPlayer) res.json({ message: "Thanks for registering!" });
+};
