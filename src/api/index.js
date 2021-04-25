e2e1450799e8cad129d492b762aac0a1061eb027
Pietro Rutzen <pietro@gramadosummit.com>
2021-04-25 18:05:41 -0300

    Use gamesApi.

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const registerApi = require("./register");
 const loginApi = require("./login");
+const gamesApi = require("./games");
 const middlewares = require("../middlewares");
 
 const router = express.Router();
 
 router.use(loginApi);
+router.use(registerApi);
 
 router.use(middlewares.checkAuth) // protects the routes below
-router.use(registerApi);
-//router.use(gameApi);
+router.use(gamesApi);
 
 module.exports = router;
